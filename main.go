e4049f24f309293b881b147bc849f4abacac8227
Adam Thomason <Adam.Thomason@findel-education.co.uk>
2020-01-08 12:15:00 +0000

    Generates haikus, but not nicely

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"time"
+	"unicode"
 
 	"github.com/ernestas-poskus/syllables"
 )
@@ -17,20 +20,22 @@ type Dictionary struct {
 func main() {
 	dictionary := generateDictionary()
 
-	for count, words := range dictionary.words {
-		fmt.Printf("%v:\n", count)
+	dictionary.generateHaiku()
+}
 
-		for _, word := range words {
-			fmt.Printf("    %v\n", word)
-		}
+func ucFirst(phrase string) string {
+	for i, v := range phrase {
+		return string(unicode.ToUpper(v)) + phrase[i+1:]
 	}
+
+	return ""
 }
 
 func generateDictionary() Dictionary {
 	dictionary := Dictionary{}
-	words := make(map[int][]string)
+	words := make(map[int][]string, 0)
 
-	file, err := os.Open("test.txt")
+	file, err := os.Open("dictionary.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,7 +44,10 @@ func generateDictionary() Dictionary {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		count := syllables.CountSyllables([]byte(scanner.Text()))
-		words[count] = append(words[count], scanner.Text())
+
+		if count <= 7 {
+			words[count] = append(words[count], scanner.Text())
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -49,4 +57,32 @@ func generateDictionary() Dictionary {
 	dictionary.words = words
 
 	return dictionary
+}
+
+func (dictionary *Dictionary) generateHaiku() {
+	fmt.Println(dictionary.generateLine(5))
+	fmt.Println(dictionary.generateLine(7))
+	fmt.Println(dictionary.generateLine(5))
+}
+
+func (dictionary *Dictionary) generateLine(syllableCount int) string {
+	var remainingCount = syllableCount
+	line := ""
+
+	for remainingCount > 0 {
+		time.Sleep(time.Millisecond * 300)
+		rand.Seed(time.Now().UnixNano())
+		randomSyllable := rand.Intn(remainingCount) + 1
+
+		max := len(dictionary.words[randomSyllable])
+
+		rand.Seed(time.Now().UnixNano())
+		randomWord := rand.Intn(max)
+
+		line = line + dictionary.words[randomSyllable][randomWord] + " "
+
+		remainingCount -= randomSyllable
+	}
+
+	return ucFirst(line)
 }
